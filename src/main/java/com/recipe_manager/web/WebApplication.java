1afdfb00a262e2ea32ab20a92fd199c25a75d026
Keshav Agrawal <keshav@Keshavs-Air.lan>
2024-06-27 23:16:42 +0100

    init commit

---
+++ src/main/java/com/recipe_manager/web/WebApplication.java
@@ -0,0 +1,13 @@
+package com.recipe_manager.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
